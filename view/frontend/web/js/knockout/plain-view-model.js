f7f31aad171370a26dddacc07114cedeb9fa835d
Ivan Matozan <ivan.matozan@inchoo.net>
2019-07-27 20:13:59 +0200

    JavaScript KnockoutJS observable

--- view/frontend/web/js/knockout/plain-view-model.js
+++ view/frontend/web/js/knockout/plain-view-model.js
@@ -1,9 +1,11 @@
-define(function () {
+define([
+    'ko'
+], function (ko) {
     'use strict';
 
     return function (config) {
         return {
-            title: 'This is a simple view-model',
+            title: ko.observable('This is a simple view-model'),
             config: config
         };
     }
