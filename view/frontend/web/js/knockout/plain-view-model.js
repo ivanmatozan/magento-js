67d202e4f2190f97e83af32b4d71bc1273bca132
Ivan Matozan <ivan.matozan@inchoo.net>
2019-07-27 20:42:18 +0200

    JavaScript KnockoutJS computed observable

--- view/frontend/web/js/knockout/plain-view-model.js
+++ view/frontend/web/js/knockout/plain-view-model.js
@@ -1,10 +1,14 @@
 define([
-    'ko'
-], function (ko) {
+    'ko',
+    'jquery'
+], function (ko, $) {
     'use strict';
 
     return function (config) {
         const title = ko.observable('This is a simple view-model');
+        let currencyInfo = ko.observable();
+        
+        $.getJSON(config.baseUrl + 'rest/V1/directory/currency', currencyInfo);
 
         title.subscribe(function (newValue) {
             console.log('Changed to', newValue);
@@ -14,9 +18,20 @@ define([
             console.log('Will be changed from', oldValue);
         }, this, 'beforeChange');
 
-        return {
+        const viewModel = {
             title: title,
-            config: config
+            config: config,
+            label: ko.observable('Currency Info')
         };
+
+        viewModel.output = ko.computed(function () {
+            if (currencyInfo()) {
+                return this.label() + ':\n' + JSON.stringify(currencyInfo(), null, 4);
+            }
+
+            return '...loading';
+        }.bind(viewModel));
+
+        return viewModel;
     }
 });
