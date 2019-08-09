afa1ec81bf2585248d467b79d650c228508c616a
Ivan Matozan <ivan.matozan@gmail.com>
2019-08-09 21:39:53 +0200

    Custom KO binding with virtual elements

--- view/frontend/web/js/ko-binding/pixelbg.js
+++ view/frontend/web/js/ko-binding/pixelbg.js
@@ -4,6 +4,32 @@ define([
 ], function (ko, pixelBackground) {
     'use strict';
 
+    const TYPE_COMMENT = 8;
+
+    /**
+     * Check if element is comment (ko virtual element)
+     *
+     * @param node
+     * @returns {boolean}
+     */
+    function isVirtualElement(node) {
+        return node.nodeType === TYPE_COMMENT;
+    }
+
+    function makeRealElement(virtualElement) {
+        const div = document.createElement('div');
+        const children = ko.virtualElements.childNodes(virtualElement);
+        ko.virtualElements.emptyNode(virtualElement);
+
+        for (let i = 0; i < children.length; i++) {
+            div.appendChild(children[i]);
+        }
+
+        ko.virtualElements.insertAfter(virtualElement, div);
+
+        return div;
+    }
+
     function execute(node, valueAccessor, allBindings, maybeCanvas) {
         const value = ko.unwrap(valueAccessor()) || {};
         const config = {
@@ -19,16 +45,21 @@ define([
     ko.bindingHandlers.pixelbg = {
         // Init handler
         init: function (node, valueAccessor, allBindings, viewModel, bindingContext) {
-            const canvas = execute(node, valueAccessor, allBindings);
-            bindingContext.$data.pixelbg = {canvas: canvas};
+            const element = isVirtualElement(node) ? makeRealElement(node) : node;
+            const canvas = execute(element, valueAccessor, allBindings);
+            bindingContext.$data.pixelbg = {canvas: canvas, element: element};
         },
 
         // Update handler
         update: function (node, valueAccessor, allBindings, viewModel, bindingContext) {
+            const element = bindingContext.$data.pixelbg.element;
             const canvas = bindingContext.$data.pixelbg.canvas;
-            execute(node, valueAccessor, allBindings, canvas);
+            execute(element, valueAccessor, allBindings, canvas);
         }
     };
+
+    // Make binding available for use by virtual elements
+    ko.virtualElements.allowedBindings.pixelbg = true;
 
     return ko;
 });
